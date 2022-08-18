ef78d5cfe242dd499e13af6fd029c402c2e74e35
tatotux <tatotux@pm.me>
2022-08-18 17:03:23 +0000

    Adding create new domain code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#create-new-domain").addEventListener("click", () => {
+    document.querySelector("#new-domain").innerHTML = generateDomain();
+  });
+};
+
+let generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let adjIndx = Math.floor(Math.random() * adj.length);
+  let nounIndx = Math.floor(Math.random() * noun.length);
+
+  return pronoun[proIndx] + adj[adjIndx] + noun[nounIndx] + ".com";
 };
